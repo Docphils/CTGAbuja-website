3ffe00907ba26f343e89de9caa72cd8bc9173a05
Philip Nwachukwu <docphils64@gmail.com>
2024-05-15 16:37:50 +0100

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@include('layouts.header');
+                    <main class="mt-6">
+                    </main>
+                    @include("layouts.footer");
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+            <footer class="py-16 pb-4 mb-3 text-center text-md text-yellow-400 dark:text-white/70">
+                <div class="">&copy; CTGAbuja 2024. All rights reserved.</div>
+                <div class="text-end text-yellow-200"> Designed By: Meph Consults</div>
+            </footer>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
