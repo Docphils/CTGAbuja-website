b4707b2a7bc9f7bccc6ccdc26694d53e15e4346b
Philip Nwachukwu <docphils64@gmail.com>
2024-06-18 09:47:23 +0100

    Service provider modification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,10 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
         Gate::define('admin', function (User $user) {
             return $user->role === 'admin';
         });
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
         }
+    }
+
     
 }
